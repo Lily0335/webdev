e2f442768c4252338907445a42bd1ad309d9c871
laraib Anwar <158305061+Lily0335@users.noreply.github.com>
2025-01-28 06:40:19 -1200

    Update 05_string.js

--- javascript/05_string.js
+++ javascript/05_string.js
@@ -1,1 +1,22 @@
+const myname = "laraib"
+const semester = 4
 
+console.log(`my name is ${myname} and my semester is ${semester}`)
+
+const newstring = "alibaba_store"
+
+console.log(newstring.toUpperCase())
+
+console.log(newstring.charAt(3))
+
+const newstring01 ="   net_flix   "
+
+console.log(newstring01.trim())
+
+console.log(newstring01.replace(" ", "_"))
+
+console.log(newstring01.split("_"))
+
+const url = "http://laraib.com/code/tech"
+
+console.log(url.includes("alibaba"))

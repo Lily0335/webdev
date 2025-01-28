056a15b7b6bcd698fc235191be384fc46c9bfd30
laraib Anwar <158305061+Lily0335@users.noreply.github.com>
2025-01-28 06:40:51 -1200

    Update 06_numbers.js

--- javascript/06_numbers.js
+++ javascript/06_numbers.js
@@ -1,1 +1,23 @@
+let score = 100
 
+console.log(score)
+
+let totalnumbers = new Number(900)
+
+console.log(totalnumbers)
+
+console.log(totalnumbers.toString())
+
+console.log(typeof totalnumbers)
+
+console.log(totalnumbers.toString().length)
+
+let anothernmber = 78.99
+
+console.log(anothernmber.toFixed(2))
+
+console.log(anothernmber.toPrecision(3))
+
+let othernumber = 10000000
+
+console.log(othernumber.toLocaleString())

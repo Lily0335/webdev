fecc051453e0989f1a1f986c46c2985bbb31c3b0
laraib Anwar <158305061+Lily0335@users.noreply.github.com>
2025-01-29 05:51:09 -1200

    Update 07_math.js

--- javascript/07_math.js
+++ javascript/07_math.js
@@ -1,1 +1,28 @@
+console.log(Math);
+console.log(Math.round(4.8));
 
+console.log(Math.PI);
+
+console.log(Math.abs(-10));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.sqrt(16));
+
+console.log(Math.min(10, 5, 8, 2, 15));
+
+console.log(Math.max(10, 5, 8, 2, 15));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+console.log(Math.ceil(Math.random() * 10) + 1);
+
+let min = 10
+
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+console.log(Math.ceil(Math.random() * (max - min + 1)) + min);

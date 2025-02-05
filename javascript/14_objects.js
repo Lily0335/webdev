c2a3903786be41b12c137e73b9f0396296824274
laraib Anwar <158305061+Lily0335@users.noreply.github.com>
2025-02-05 06:52:52 -1200

    Update 14_objects.js

--- javascript/14_objects.js
+++ javascript/14_objects.js
@@ -1,1 +1,19 @@
 
+const mysym = Symbol("key1");
+
+const objects ={
+    name: "laraib",
+    [mysym]: "mykey2",
+    age: "19",
+    country: "Pakistan",
+    city: "Karachi",
+    hobbies: ["reading", "painting", "traveling"],
+    
+}
+
+console.log(objects.name);
+
+console.log(objects["name"]);
+
+console.log( objects[mysym]);
+console.log(objects);
